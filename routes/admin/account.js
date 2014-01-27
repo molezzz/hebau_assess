e45ee774d22ae4f7285dff70cc80b23ac31c589d
molezz <molezzz@gmail.com>
2014-01-27 17:33:50 +0800

    完成账户生成 完成账户删除和禁用、解封

--- routes/admin/account.js
+++ routes/admin/account.js
@@ -4,6 +4,7 @@
 var ex = require('lodash');
 var moment = require('moment');
 var orm = require('../../lib/seq-models');
+var rnds = require('randomstring');
 
 var shared = {
   accountMenuActive: 'active'
@@ -17,6 +18,7 @@ exports.index = function(req, res){
       case 'json':
         var opts = Account.pages(req.query.page, req.query.prepage);
         opts.order = 'id DESC';
+        opts.include = [{ model: Department, as: 'department'}],
         Account.search(req.query.q || {}, opts).success(function(accounts){
           res.json({ total: accounts.count, accounts: accounts.rows });
         });
@@ -57,13 +59,51 @@ exports.wizardRun = function(req, res){
   var Account = orm.model('account');
   var io = res.socketSrv;
   var cid = req.body['cid'];
-  setTimeout(function(){
-    io.clients[cid].emit('account_status',{msg: '正在分组数据'});
-    setTimeout(function(){
-      io.clients[cid].emit('account_status',{msg: '已生成 100个'});
-      res.json({});
-    },5000);
-  },1000);
+  var deps = req.body['deps'];
+  var records = {};
+  var total = 0;
+  var groupKey = moment().format('YYYYMMDD');
+  io.clients[cid].emit('account_status',{msg: '正在分组数据'});
+  for(var id in deps){
+    var prefix = rnds.generate(2);
+    records[id] = [];
+    for (var i = 0; i < deps[id]; i++) {
+      total++;
+      records[id].push({
+        name: (prefix + '-' + rnds.generate(6)).toLowerCase(),
+        department_id: id,
+        group_key: groupKey,
+        invalid: false
+      });
+    };
+  };
+  io.clients[cid].emit('account_status',{msg: '账号已生成，总计: '+ total +'个，开始写入数据库。', total: total});
+  //console.log(records);
+  var chainer = new (orm.Seq().Utils.QueryChainer);
+  ex.forEach(records, function(accounts, key){
+    chainer.add(Account, 'bulkCreate', [accounts],{
+      before: function(model) {
+        io.clients[cid].emit('account_status',{msg: '正在保存“'+ key + '组"'});
+      },
+      success: function(migration, next) {
+        io.clients[cid].emit('account_status',{msg: '已保存“'+ key + '组”共' + accounts.length +'个', count: accounts.length});
+        setTimeout(function(){next();}, 500);
+      }
+    });
+  });
+  chainer
+  .runSerially({ skipOnError: true })
+  .success(function(){
+    io.clients[cid].emit('account_status',{msg: '已生成 '+ total +'个'});
+    res.json({ success: true });
+  });
+  // setTimeout(function(){
+  //   io.clients[cid].emit('account_status',{msg: '正在分组数据'});
+  //   setTimeout(function(){
+  //     io.clients[cid].emit('account_status',{msg: '已生成 100个'});
+  //     res.json({});
+  //   },5000);
+  // },1000);
 };
 
 exports.new = function(req, res){
@@ -108,10 +148,11 @@ exports.update = function(req, res){
   var result = {success: false, msg: ''};
 
   Account.find(accountId).success(function(account){
-    account.updateAttributes(req.body.account, ['name', 'category_id', 'parent_id'])
-        .success(function(){
+    account.updateAttributes(req.body.account, ['invalid'])
+        .success(function(account){
           result.msg = '更新成功';
           result.success = true;
+          result.account = account;
           res.json(result);
         })
         .error(function(errors){
@@ -121,7 +162,7 @@ exports.update = function(req, res){
         });
 
   }).error(function(errors){
-    result.msg = '更新失败! 修改的部门不存在！';
+    result.msg = '更新失败! 账户不存在！';
     console.log(errors);
     res.json(result);
   });
@@ -132,9 +173,10 @@ exports.destroy = function(req, res){
   var accounts = req.params.account.split('-');
   var Account = orm.model('account');
   var result = {success: false, msg: ''};
+  //TODO: 删除投票
   Account.destroy({id: accounts})
       .success(function(){
-        result.msg = '部门已删除！';
+        result.msg = '账户已删除！';
         result.success = true;
         res.json(result);
       })
